6ff38c7658d369a2d71861bc69bcb92d4d91b031
Jason Sjambar <realjase@gmail.com>
2015-10-04 07:58:11 -0700

    Better version of assignment

--- Assignment5_Exc2/Assignment5_Exc2/Assignment5_Exc2.py
+++ Assignment5_Exc2/Assignment5_Exc2/Assignment5_Exc2.py
@@ -1,28 +1,35 @@
-# Import regular expression module
-import re
-# Allow any decimal digit
-containsNumbers = re.compile('\d')
-# Get user input
-userInput = input("Please enter a password")
+﻿# Get user input
+userInput = input("Please enter a password: ")
 # Print user input
 print("Your entered password was: "+userInput)
+points = 0
 
 # Get string length
 strLen = len(userInput)
+# Fancy characters
+fancyCharacters = "!@#$%^&_-"
 
-# If string length is less than 8
-if(strLen < 8):
-    print("Password is too short, which is bad.")
-else:
-    # If the entire string is lower case
-    if(userInput.islower()):
-        print("Password is bad, please add capitalized letters.")
-    # If the string contains a number, return value as boolean(t/f)
-    elif(bool(containsNumbers)):
-        print("Great password!")
-        validPassword = True
-    else:
-    # If the string didn't contain a capitalized letter AND number
-        print("Please add capitalized letters, numbers and some stupid characters.")
+if(strLen >= 8):
+    points += 10
+
+for char in userInput:
+    if(char.islower()):
+        points += 1
+    elif(char.isupper()):
+        points += 2
+    elif(char.isdigit()):
+        points += 2
+    elif(char in fancyCharacters):
+        points += 3
+
+print("Points: "+str(points))
+
+if(points < 20):
+    print("Password is garbage.")
+elif(points > 20 and points < 30):
+    print("Password is decent!")
+elif(points > 30):
+    print("Password is good.")
+
 
 
