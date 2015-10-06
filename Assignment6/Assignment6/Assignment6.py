7d3b0248edb08301af3cf374f46f6a8566004ab3
Jason Sjambar <realjase@gmail.com>
2015-10-06 03:41:12 -0700

    Smiley completed

--- Assignment6/Assignment6/Assignment6.py
+++ Assignment6/Assignment6/Assignment6.py
@@ -4,6 +4,7 @@ width           = int(input("What should be the width of the figures? "))
 figureString    = ""
 
 '''
+
 # A full square
 print("A full square.")
 
@@ -21,6 +22,9 @@ print("A hollow square.")
 
 for y in range(0, height):
 
+    # We do (height - 1) because the loop ends at index (height - 1).
+    # We check if we're at the first index or at the last and just create the entire width of the cube at the top and bottom
+    # Else we check if we're at the first or last index of the width and add a star there.
     if (y == 0 or y == (height - 1) ):
         for x in range(0, width):
             figureString += "*"
@@ -41,11 +45,10 @@ figureString = ""
 # A full rectangle triangle
 print("A full rectangle triangle.")
 
-
 for x in range(0, width):
  
     # x + 1 causes the x to get increased by 1, which means that we get no space at the first line and we still get 5 stars at the bottom!
-    for x_width in range(0, (x+1)):
+    for x_width in range(0, (x + 1)):
         figureString += "*"
 
     figureString += "\n"
@@ -57,7 +60,7 @@ figureString = ""
 # A full isosceles triangle
 print("A full isosceles triangle.")
 
-for y in range(0, height):
+for y in range(1, height):
     # Spaces = width - current index. This way we know how many spaces we need before the stars
     spaces = height - y
     # Stars = width - spaces. We removed the amount of spaces we have, so we know how many stars we need to show.
@@ -70,19 +73,16 @@ for y in range(0, height):
     #print("stars = "+str(stars))
 
     # So we don't get the first empty line cause of index 0
-    if(y > 0):
+    for space in range(0, spaces):
+        figureString += " "
 
-        for space in range(0, spaces):
-            figureString += " "
+    for star in range(0, stars):
+        figureString += "*"
 
-        for star in range(0, stars):
-            figureString += "*"
-
-        figureString += "\n"
+    figureString += "\n"
 
 print(figureString)
 figureString = ""
-'''
 
 
 # A full circle.
@@ -114,9 +114,47 @@ for y in range(0, width):
 print(figureString)
 figureString = ""
 
+'''
+
+# A full circle.
+print("A full circle.")
+
+# Center
+center = width / 2
+
+for y in range(0, width):
+    # Get the distance between the center and current index (y)
+    distance_y = (center - y) * (center - y)
+    for x in range(0, width):
+        # Get the distance between the center and the current index (x)
+        distance_x = (center - x) * (center - x)
+        # Calculate the total distance by adding the y-axis and x-axis distance and using the square root on this (because we did value * 2 on distance_y and distance_x)
+        distance = math.sqrt(distance_y + distance_x)
+
+        # distance = 0 = center and thats for the nose
+        if(distance == 0):
+            figureString += "#"
+        # eyes
+        elif(distance == 5 and y == (width / 2.5)):
+            figureString += "o"
+        # smiley
+        elif(distance == 5 and y > center):
+            figureString += "-"
+        # face 
+        elif(distance < center and distance > (center / 1.2)): 
+            figureString += "*"
+        else:
+            figureString += " "
+        
+
+    figureString += "\n"
+
+print(figureString)
+figureString = ""
+
 
 '''
-# Ugly face.
+# Shitty written ugly face.
 for y in range(0, 10):
     for x in range(0, 10):
         if((y == 0 or y == 8) and x > 3 and x < 7):
