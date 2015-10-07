14917fd285a446701ff55c3add4b2ebf433c3513
Jason Sjambar <realjase@gmail.com>
2015-10-07 01:04:26 -0700

    fixed explanation

--- Assignment5_Exc1/Assignment5_Exc1/Assignment5_Exc1.py
+++ Assignment5_Exc1/Assignment5_Exc1/Assignment5_Exc1.py
@@ -1,9 +1,8 @@
-userInput = input("Please enter the text you wish to get returned backwards.")
+﻿userInput = input("Please enter the text you wish to get returned backwards.")
 # Gives the reverse value back.
-# If we were to use [::5], it would return us the values with the index 5
-# Using - we get the reversed value returned.
-# When you use 1, you get every letter with the index 1, which is everything.
-# Combine - for the reversed value and 1 to get everything, leads to getting the entire value returned backwards.
+# [start:end:steps]
+# We dont enter a start and end value, so we grab the entire string.
+# By using -1 we go 1 step back and get the string in a reversed way.
 reversedInput = userInput[::-1]
 
 print(reversedInput)
